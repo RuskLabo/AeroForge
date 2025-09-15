5852957c7dbb91c90241e4d81c996a55c6fb34ed
RuskLabo <83647593+RuskLabo@users.noreply.github.com>
2025-09-15 20:47:01 +0900

    First Commit

---
+++ src/main/java/com/rusklabo/aeroForge/AeroForge.java
@@ -0,0 +1,17 @@
+package com.rusklabo.aeroForge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AeroForge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
